dd9c1d79717f24e8733e1744eafbfcfffab5cca3
Steve <johnw@motif.foundation>
2022-01-25 20:43:02 +0300

    changes

--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,6 @@
  
-import { MotifSpace20210905 } from './motif/MotifSpace20210905';  
+import { MotifLand20210905 } from './motif/MotifLand20210905';  
  
-export { MotifSpace20210905 } 
+export { MotifLand20210905 } 
 
-export type MetadataLike = MotifSpace20210905;
+export type MetadataLike = MotifLand20210905;
